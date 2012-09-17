895e095ebd7b6be92c5ff003f9478b73597bf4c7
David Campbell <djcampbell2@cougars.ccis.edu>
2012-09-17 01:34:58 -0500

    Finished ciss375

--- ciss/ciss375/a3/file.cpp
+++ ciss/ciss375/a3/file.cpp
@@ -9,8 +9,9 @@ using namespace std;
 
 bool is_op(char value)
 {
-	if (value == '+' || value == '-' || value == '*' || value == '/')
+	if (value == 42 || value == 43 || value == 45 || value == 47)
 		return true;
+	return false;
 }
 
 bool is_less_than_or_eq(char value1, char value2)
@@ -33,22 +34,19 @@ int main(int argc, char * argv[])
 	for (int i = 0; i < input_line.size(); i++) {
 		if (input_line[i] != ' ') {
 			cv.push_back(input_line[i]);
-			
-			// Create the names for the nodes
-			//stringstream out;
-			//out << cv.size() - 1;
-			//name.push_back("n" + out.str());
 		}
 	}
 
-	// Parse the tree into RPN
+	// Shunting yard algorithm
 	stack<char> op_stack;
 	vector<char> output;
 
 	for (int i = 0; i < cv.size(); i++)
 	{
 		if (cv[i] >= '1' && cv[i] <= '9')
+		{
 			output.push_back(cv[i]);
+		}
 		else if (is_op(cv[i]))
 		{
 			if (op_stack.size() == 0)
@@ -59,7 +57,7 @@ int main(int argc, char * argv[])
 				{
 					for (int j = op_stack.size(); j > 0; j--)
 					{
-						if (is_less_than_or_eq(cv[i], op_stack.top()))
+						if (is_less_than_or_eq(cv[i], op_stack.top()) && op_stack.top() != '(')
 						{
 							output.push_back(op_stack.top());
 							op_stack.pop();
@@ -77,19 +75,28 @@ int main(int argc, char * argv[])
 		}
 		else if (cv[i] == '(')
 		{
-			op_stack.push('(');
+			op_stack.push(cv[i]);
 		}
 		else if (cv[i] == ')')
 		{
-			while (op_stack.top() != '(')
+			for (int j = op_stack.size(); j > 0; j--)
 			{
-				output.push_back(op_stack.top());
-				op_stack.pop();
+				if (op_stack.top() != '(')
+				{
+					output.push_back(op_stack.top());
+					op_stack.pop();
+				}
+				else
+					break;
 			}
 			op_stack.pop();
 		}
 	}
 
+	for (int i = 0; i < name.size(); i++)
+		cout << name[i] << ' ';
+
+	// Add the extra operators onto the end of the output stream
 	for (int i = 0; i < op_stack.size(); i++)
 	{
 		output.push_back(op_stack.top());
@@ -98,36 +105,52 @@ int main(int argc, char * argv[])
 			output.push_back(op_stack.top());
 	}
 
+	// Print the output stream
 	for (int i = 0; i < output.size(); i++)
-		cout << output[i] << endl;
+		cout << output[i];
 
-	/*ofstream file;
+	// Create names for nodes
+	for (int i = 0; i < output.size(); i++)
+	{
+		stringstream out;
+		out << i;
+		name.push_back("n" + out.str());
+	}
+
+	ofstream file;
 	file.open("graph.dot");
 	file << "digraph G {\n\t";
 	file.close();
 	file.open("graph.dot", ios::app);
 
-	for (int i = 0; i < cv.size(); i++) {
-		//cout << cv[i] << endl;
-		if ((cv[i] == '*' || cv[i] == '/' || cv[i] == '+' || cv[i] == '-') &&
-			(cv[i - 1] == '*' || cv[i - 1] == '/' || cv[i - 1] == '+' || cv[i - 1] == '-')) {
-			file << name[i] << " [label = \"" << cv[i] << "\"];\n\t";
-			file << name[i - 1] << " [label = \"" << cv[i - 1] << "\"];\n\t";
+	stack<string> label;
 
-			file << name[i] << "->" << name[i - 1] << "\n\t";
-			file << name[i] << "->" << name[i - 4] << "\n\t";
+	// Label the values of the nodes
+	for (int i = 0; i < name.size(); i++)
+	{
+		file << name[i] << " [label = \"" << output[i] << "\"]\n\t";
+	}
+
+	// Used notes from http://www.docstoc.com/docs/55696108/Chapter-9-binary-tree
+	// Create the tree
+	for (int i = 0; i < output.size(); i++)
+	{
+		if (!is_op(output[i]))
+		{
+			label.push(name[i]);
 		}
-		else if (cv[i] == '*' || cv[i] == '/' || cv[i] == '+' || cv[i] == '-') {
-			file << name[i] << " [label = \"" << cv[i] << "\"];\n\t";
-			file << name[i - 1] << " [label = \"" << cv[i - 1] << "\"];\n\t";
-			file << name[i - 2] << " [label = \"" << cv[i - 2] << "\"];\n\t";
-
-			file << name[i] << "->" << name[i - 1] << "\n\t";
-			file << name[i] << "->" << name[i - 2] << "\n\t";
+		else
+		{
+			file << name[i] << " -> " << label.top() << "\n\t";
+			label.pop();
+			file << name[i] << " -> " << label.top() << "\n\t";
+			label.pop();
+			label.push(name[i]);
 		}
 	}
+
 	file << "\n}";
-	file.close();*/
+	file.close();
 
 	return 0;
 }
