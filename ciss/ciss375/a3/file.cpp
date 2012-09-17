e651eb5d1bf4500ce0bdbe9ceffed78042a4bc39
David Campbell <david@goose.ccis.edu>
2012-09-16 20:38:59 -0500

    Working on ciss375 a3

--- ciss/ciss375/a3/file.cpp
+++ ciss/ciss375/a3/file.cpp
@@ -3,8 +3,25 @@
 #include <cstring>
 #include <sstream>
 #include <fstream>
+#include <stack>
 
 using namespace std;
+
+bool is_op(char value)
+{
+	if (value == '+' || value == '-' || value == '*' || value == '/')
+		return true;
+}
+
+bool is_less_than_or_eq(char value1, char value2)
+{
+	if (value1 == '/' || value1 == '*')
+	{
+		if (value2 == '+' || value2 == '-')
+			return false;
+	}
+	return true;
+}
 
 int main(int argc, char * argv[])
 {
@@ -16,15 +33,75 @@ int main(int argc, char * argv[])
 	for (int i = 0; i < input_line.size(); i++) {
 		if (input_line[i] != ' ') {
 			cv.push_back(input_line[i]);
-
+			
 			// Create the names for the nodes
-			stringstream out;
-			out << cv.size() - 1;
-			name.push_back("n" + out.str());
+			//stringstream out;
+			//out << cv.size() - 1;
+			//name.push_back("n" + out.str());
 		}
 	}
 
-	ofstream file;
+	// Parse the tree into RPN
+	stack<char> op_stack;
+	vector<char> output;
+
+	for (int i = 0; i < cv.size(); i++)
+	{
+		if (cv[i] >= '1' && cv[i] <= '9')
+			output.push_back(cv[i]);
+		else if (is_op(cv[i]))
+		{
+			if (op_stack.size() == 0)
+				op_stack.push(cv[i]);
+			else
+			{
+				if (is_less_than_or_eq(cv[i], op_stack.top()))
+				{
+					for (int j = op_stack.size(); j > 0; j--)
+					{
+						if (is_less_than_or_eq(cv[i], op_stack.top()))
+						{
+							output.push_back(op_stack.top());
+							op_stack.pop();
+						}
+						else
+							break;
+					}
+					op_stack.push(cv[i]);
+				}
+				else
+				{
+					op_stack.push(cv[i]);
+				}
+			}
+		}
+		else if (cv[i] == '(')
+		{
+			op_stack.push('(');
+		}
+		else if (cv[i] == ')')
+		{
+			while (op_stack.top() != '(')
+			{
+				output.push_back(op_stack.top());
+				op_stack.pop();
+			}
+			op_stack.pop();
+		}
+	}
+
+	for (int i = 0; i < op_stack.size(); i++)
+	{
+		output.push_back(op_stack.top());
+		op_stack.pop();
+		if (op_stack.size() == 1)
+			output.push_back(op_stack.top());
+	}
+
+	for (int i = 0; i < output.size(); i++)
+		cout << output[i] << endl;
+
+	/*ofstream file;
 	file.open("graph.dot");
 	file << "digraph G {\n\t";
 	file.close();
@@ -50,7 +127,7 @@ int main(int argc, char * argv[])
 		}
 	}
 	file << "\n}";
-	file.close();
+	file.close();*/
 
 	return 0;
 }
