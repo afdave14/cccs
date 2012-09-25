b36521223ac2a848d8fc76bbfb41be7c0fb05828
David Campbell <djcampbell2@cougars.ccis.edu>
2012-09-24 21:32:46 -0500

    Added ruby

---
+++ ruby/say.rubyprograms/p002rubynumbers.rb
@@ -0,0 +1,19 @@
+#p002rubynumbers.rb
+=begin
+	
+Ruby numbers
+Usual operators:
++ addition
+- subtraction
+* multiplication
+/ division
+	
+=end
+
+puts 1 + 2
+puts 2 * 3
+# Integer division
+# When you do arithmetic with integers you will get integer results
+puts 3 / 2
+puts 10 - 11
+puts 1.5 / 1.6
