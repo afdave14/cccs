941d4f2dbe2d9fef8ad08fc71da93d8e9af285c9
David Campbell <djcampbell2@cougars.ccis.edu>
2013-01-24 22:11:55 -0600

    Added sabermetrics files

---
+++ ciss/ciss438/project/site/shorty/app/models/url.rb
@@ -0,0 +1,5 @@
+class Url < ActiveRecord::Base
+	attr_accessible :url
+	
+	validates :url, presence: true
+end
---
+++ ciss/ciss438/project/site/shorty/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shorty::Application.config.secret_token = '6765de959105efd74969d5dc5af26d5df078901096098394b160e7e19e7c7d7fc0d1385fb7da2c45467b9b92e1943f609483286e15e7ae14eaebbb19e9075f82'
